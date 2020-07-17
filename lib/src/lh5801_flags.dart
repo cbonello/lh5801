2c281a027a548abc40977a71916303653e3f396e
Christophe Bonello <cbonello@users.noreply.github.com>
2020-07-17 15:06:33 -0400

    save flasg as bolleans rather than integers

--- lib/src/lh5801_flags.dart
+++ lib/src/lh5801_flags.dart
@@ -7,25 +7,35 @@ part 'lh5801_flags.g.dart';
   disallowUnrecognizedKeys: true,
 )
 class LH5801Flags {
-  LH5801Flags({this.h = 0, this.v = 0, this.z = 0, this.ie = 0, this.c = 0});
+  LH5801Flags(
+      {this.h = false, this.v = false, this.z = false, this.ie = false, this.c = false});
 
   factory LH5801Flags.fromJson(Map<String, dynamic> json) => _$LH5801FlagsFromJson(json);
 
   Map<String, dynamic> toJson() => _$LH5801FlagsToJson(this);
 
-  int h, v, z, ie, c;
+  bool h, v, z, ie, c;
 
-  int get statusRegister => (h << 4) | (v << 3) | (z << 2) | (ie << 1) | c;
+  int get statusRegister =>
+      (boolToInt(h) << 4) |
+      (boolToInt(v) << 3) |
+      (boolToInt(z) << 2) |
+      (boolToInt(ie) << 1) |
+      boolToInt(c);
 
   set statusRegister(int flags) {
-    h = (flags & 0x10) >> 4;
-    v = (flags & 0x08) >> 3;
-    z = (flags & 0x04) >> 2;
-    ie = (flags & 0x02) >> 1;
-    c = flags & 0x01;
+    h = intToBool((flags & 0x10) >> 4);
+    v = intToBool((flags & 0x08) >> 3);
+    z = intToBool((flags & 0x04) >> 2);
+    ie = intToBool((flags & 0x02) >> 1);
+    c = intToBool(flags & 0x01);
   }
 
   void reset() => statusRegister = 0;
 
   LH5801Flags clone() => LH5801Flags(h: h, v: v, z: z, ie: ie, c: c);
+
+  // ignore: avoid_positional_boolean_parameters
+  int boolToInt(bool value) => value ? 1 : 0;
+  bool intToBool(int value) => value != 0;
 }
