bbc8a9c7af07526cf54698595e63a0de03ebb901
Christophe Bonello <cbonello@users.noreply.github.com>
2020-07-23 18:16:54 -0400

    remove GenerateSubTable() function; we only need the Add table.

--- tools/table_generator.cpp
+++ tools/table_generator.cpp
@@ -32,19 +32,19 @@ const char *epilogue = "};\n";
 #define FLAG_V 0x08
 #define FLAG_H 0x10
 
-/****************************************************************************/
-/* Function:	Dump														*/
-/*																			*/
-/* Print formatted data to a file.											*/
-/*																			*/
-/* Parameters:	Output file descriptor.										*/
-/*				Output filename (to dump error messages).					*/
-/*				Output format and variables arguments.						*/
-/*																			*/
-/* Return:																	*/
-/*																			*/
-/* Exceptions:	EXCEPTION_Write_Error: write error.							*/
-/****************************************************************************/
+/*
+ * Function:    Dump
+ *
+ * Print formatted data to a file.
+ *
+ * Parameters:  Output file descriptor.
+ *              Output filename (to dump error messages).
+ *              Output format and variables arguments.
+ *
+ * Return:
+ *
+ * Exceptions:  EXCEPTION_Write_Error: write error.
+ */
 static void Dump(FILE *file, const char *filename, const char *format, ...)
 {
     va_list args;
@@ -58,20 +58,19 @@ static void Dump(FILE *file, const char *filename, const char *format, ...)
     va_end(args);
 }
 
-/****************************************************************************/
-/* Function:	GenerateAddTable											*/
-/*																			*/
-/* Generate a table that store the values of the C, Z, V and H status bits	*/
-/* after an ADD or ADDC operation is performed. Table is stored in a '.h'	*/
-/* file.																	*/
-/*																			*/
-/* Parameters:                              								*/
-/*																			*/
-/* Return:																	*/
-/*																			*/
-/* Exceptions:	EXCEPTION_Create_Error: unable to create output file.		*/
-/*				EXCEPTION_Write_Error: write error.							*/
-/****************************************************************************/
+/*
+ * Function:  GenerateAddTable
+ *
+ * Generate a table that store the values of the C, Z, V and H status bits
+ * after an ADD or ADDC operation is performed.
+ *
+ * Parameters:
+ *
+ * Return:
+ *
+ * Exceptions:  EXCEPTION_Create_Error: unable to create output file.
+ *              EXCEPTION_Write_Error: write error.
+ */
 static void GenerateAddTable()
 {
     const char *filename = "lh5801_add_table.dart";
@@ -130,93 +129,20 @@ static void GenerateAddTable()
     }
 }
 
-/****************************************************************************/
-/* Function:	GenerateSubTable											*/
-/*																			*/
-/* Generate a table that store the values of the C, Z, V and H status bits	*/
-/* after an SUB or SUBC operation is performed. Table is stored in a '.h'	*/
-/* file.																	*/
-/*																			*/
-/* Parameters:                              								*/
-/*																			*/
-/* Return:																	*/
-/*																			*/
-/* Exceptions:	EXCEPTION_Create_Error: unable to create output file.		*/
-/*				EXCEPTION_Write_Error: write error.							*/
-/****************************************************************************/
-static void GenerateSubTable()
-{
-    const char *filename = "lh5801_sbc_table.dart";
-
-    fprintf(stderr, "Generating '%s'...\n", filename);
-
-    FILE *file = fopen(filename, "w");
-    if (file == 0L)
-    {
-        fprintf(stderr, "%s: Error: Unable to create '%s'\n", gProgramName, filename);
-        throw EXCEPTION_Create_Error;
-    }
-
-    try
-    {
-        Dump(file, filename, prologue, "sbcTable");
-
-        for (unsigned int carry = 0; carry < 2; carry++)
-        {
-            for (unsigned int op1 = 0; op1 <= 255; op1++)
-            {
-                for (unsigned int o2 = 0; o2 <= 255; o2++)
-                {
-                    unsigned int op2 = o2 ^ 0xFF;
-                    unsigned int result = op1 + op2 + carry;
-                    int statusRegister = 0;
-
-                    if ((result & 0xFF) == 0)
-                    {
-                        statusRegister |= FLAG_Z;
-                    }
-                    if ((result & 0x100) != 0)
-                    {
-                        statusRegister |= FLAG_C;
-                    }
-                    if (((op1 & 0x80) == ((op2 + carry) & 0x80)) && ((op1 & 0x80) != (result & 0x80)))
-                    {
-                        statusRegister |= FLAG_V;
-                    }
-                    if ((((op1 & 0x0F) + (op2 & 0x0F) + carry) & 0x10) != 0)
-                    {
-                        statusRegister |= FLAG_H;
-                    }
-
-                    Dump(file, filename, itemTemplate, op1, o2, carry, result & 0xFF, statusRegister);
-                }
-            }
-        }
-
-        Dump(file, filename, epilogue);
-        fclose(file);
-    }
-    catch (Exceptions e)
-    {
-        fclose(file);
-        throw e;
-    }
-}
-
-/****************************************************************************/
-/* Function:	main														*/
-/*																			*/
-/* Entry point.																*/
-/*																			*/
-/* Parameters:	An integer that contains the count of arguments that follow */
-/*				  in 'argv'.												*/
-/*				An array of null-terminated strings representing the com-	*/
-/*				  mand-line arguments entered by the user of the program.	*/
-/*																			*/
-/* Return:		False if success, otherwise true.							*/
-/*																			*/
-/* Exceptions:																*/
-/****************************************************************************/
+/*
+ * Function:  main
+ *
+ * Entry point.
+ *
+ * Parameters:  An integer that contains the count of arguments that follow 
+ *                in 'argv'.
+ *              An array of null-terminated strings representing the com-
+ *                mand-line arguments entered by the user of the program.
+ *
+ * Return:      False if success, otherwise true.
+ *
+ * Exceptions:
+ */
 int main(int argc, char *argv[])
 {
     bool lSuccess = true;
