a81d8885bcc9fedd24384abf71d3dd1b10af6f85
Christophe Bonello <cbonello@users.noreply.github.com>
2020-07-31 09:30:33 -0400

    fix CPU cycles for ROL instruction

--- lib/src/cpu/lh5801_instruction_tables.dart
+++ lib/src/cpu/lh5801_instruction_tables.dart
@@ -3727,7 +3727,7 @@ final List<InstructionDescriptor> instructionTable = <InstructionDescriptor>[
       Operand(OperandType.none(), null),
       Operand(OperandType.none(), null),
     ],
-    CyclesCount(6, 0),
+    CyclesCount(8, 0),
   ),
   const InstructionDescriptor(
     // VEJ (DC)
