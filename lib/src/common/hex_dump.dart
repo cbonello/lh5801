909dce890ab32c88c49070e4803ec6b7e907e1b8
Christophe Bonello <cbonello@users.noreply.github.com>
2020-08-06 17:02:28 -0400

    delete unused prefix() function

--- lib/src/common/hex_dump.dart
+++ lib/src/common/hex_dump.dart
@@ -5,7 +5,5 @@ class HexDump {
   static String hex16(int value) =>
       '${value.toUnsigned(16).toRadixString(16).toUpperCase().padLeft(4, '0')}H';
 
-  static String prefix(int address) => address >= 0x10000 ? '#' : '';
-
   static String meHex16(int address) => hex16(address);
 }
